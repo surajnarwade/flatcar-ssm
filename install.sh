a261063c27c6d51f632386258a792cf976e7ffb7
Suraj Narwade <suraj.narwade@rvu.co.uk>
2021-03-06 19:00:28 +0000

    add install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# create directories
+mkdir -p /var/log/amazon/ssm
+mkdir -p /etc/amazon/ssm
+
+# clone the repo
+git clone https://github.com/surajnarwade/flatcar-ssm /tmp/flatcar-ssm
+
+# copy binaries
+cp -rf /tmp/flatcar-ssm/linux_amd64/* /usr/bin
+
+# copy amazon ssm agent json
+cp  /tmp/flatcar-ssm/amazon-ssm-agent.json.template /etc/amazon/ssm/amazon-ssm-agent.json
