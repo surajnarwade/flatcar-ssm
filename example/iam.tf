7368fd5d23b2b722111944b2d21f541cc2ec619e
Suraj Narwade <suraj.narwade@rvu.co.uk>
2021-03-07 13:54:35 +0000

    change binary dir

--- example/iam.tf
+++ example/iam.tf
@@ -1,4 +1,25 @@
-resource "aws_iam_instance_profile" "cilium_etcd" {
-  name = "test-cilium_etcd"
-  role = "test-flatcar-ssm"
+resource "aws_iam_instance_profile" "test-ssm" {
+  name = "test-ssm"
+  role = "test-ssm"
+}
+
+resource "aws_iam_role" "test-ssm" {
+  name = "test-ssm"
+  assume_role_policy = data.aws_iam_policy_document.test-ssm.json
+}
+
+data "aws_iam_policy_document" "test-ssm" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "test-ssm-AmazonSSMManagedInstanceCore" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.test-ssm.name
 }
--- example/main.tf
+++ example/main.tf
@@ -1,9 +1,10 @@
-data "aws_ami" "cloud-base-image" {
+data "aws_ami" "image" {
   most_recent = true
 
   filter {
-    name   = "name"
-    values = ["base-image-fc-b4b2c239"]
+    name = "name"
+    # values = ["base-image-fc-b4b2c239"]
+    values = ["Flatcar-stable-2079.3.0-hvm"]
   }
 
   filter {
@@ -11,19 +12,20 @@ data "aws_ami" "cloud-base-image" {
     values = ["hvm"]
   }
 
-  owners = ["477284023816"]
+  # owners = ["477284023816"]
+  owners = ["075585003325"]
 }
 
-resource "aws_instance" "cilium-etcd" {
+resource "aws_instance" "test-ssm" {
 
-  ami           = data.aws_ami.cloud-base-image.id
-  instance_type = "t2.medium" # "m4.large"
- associate_public_ip_address = true
-  iam_instance_profile = aws_iam_instance_profile.cilium_etcd.name
-  
-  subnet_id = "subnet-0649bf3af7f335405"
+  ami                         = data.aws_ami.image.id
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.test-ssm.name
 
-  vpc_security_group_ids = ["sg-0e0fad229f48b7a13"]
+  subnet_id = "subnet-02b8e305601ad20be"
+
+  vpc_security_group_ids = ["sg-0f4a2577133335c3d"]
 
   user_data = data.ct_config.ignition.rendered
 
@@ -31,7 +33,6 @@ resource "aws_instance" "cilium-etcd" {
   key_name = "suraj-keypair"
 
   tags = {
-    Name = "test-cilium-etcd"
-    Role = "test-cilium-etcd"
+    Name = "test-ssm"
   }
 }
